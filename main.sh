26d7710c2e78ad74db9289e364251600483bd7a9
xhuang31 <xhuang31@hawk.iit.edu>
2018-11-15 15:34:03 -0600

    Initial Release

--- main.sh
+++ main.sh
@@ -3,22 +3,34 @@
 echo "Downloading SimpleQuestions dataset...\n"
 wget https://www.dropbox.com/s/9lxudhdfpfkihr1/data.zip
 unzip data.zip
-
-echo "Downloading Knowledge Graph Embedding...\n"
-wget https://www.dropbox.com/s/90l0xony07s1ybq/preprocess.zip
-unzip preprocess.zip
+rm data.zip
 
 echo "Preprocess the raw data"
 python3.6 trim_names.py -f data/freebase-FB2M.txt -n data/FB5M.name.txt
 
-echo "\n\nCreate processed, augmented dataset...\n"
+echo "Create processed, augmented dataset...\n"
 python3.6 augment_process_dataset.py -d data/
 
 
-echo "Entity Detection, Train and test the model"
+echo "Embed the Knowledge Graph:\n"
+echo "It takes too long time and an existing method is used. Thus, we download the Knowledge Graph Embedding directly...\n"
+wget https://www.dropbox.com/s/o5hd8lnr5c0l6hj/KGembed.zip
+unzip KGembed.zip
+rm KGembed.zip
+
+mv -f KGembed/* preprocess/
+rm -r KGembed
+
+#python3.6 transE_emb.py --learning_rate 0.003 --batch_size 3000 --eval_freq 50
+
+
+
+
+
+echo "Head Entity Detection (HED) model, train and test the model..."
 python3.6 train_detection.py --entity_detection_mode LSTM --fix_embed --gpu 0
 
-echo "Entity representation learning"
+echo "Entity representation learning..."
 python3.6 train_entity.py --qa_mode LSTM --fix_embed --gpu 0
 python3.6 train_pred.py --qa_mode LSTM --fix_embed --gpu 0
 python3.6 test_main.py --gpu 0
